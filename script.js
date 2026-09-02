c1dd560972cc1bba01f30c2a6352ee7e1a1bc16c
nimmalaneha26-loyal <nimmalaneha26@gmail.com>
2026-09-02 14:44:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+var selfRatings = {};
+
+window.onload = function() {
+var explainSection = document.getElementById("explain-section");
+
+for (var skill in explainQuestions) {
+var block = document.createElement("div");
+block.className = "explain-block";
+
+var questionText = document.createElement("p");
+questionText.innerHTML = "<strong>" + skill + ":</strong> " + explainQuestions[skill];
+block.appendChild(questionText);
+
+var clearBtn = document.createElement("button");
+clearBtn.type = "button";
+clearBtn.className = "rate-btn";
+clearBtn.innerHTML = "I can explain this clearly";
+clearBtn.setAttribute("data-skill", skill);
+clearBtn.onclick = function() {
+var s = this.getAttribute("data-skill");
+selfRatings[s] = "clear";
+highlightSelected(this);
+};
+
+var struggleBtn = document.createElement("button");
+struggleBtn.type = "button";
+struggleBtn.className = "rate-btn";
+struggleBtn.innerHTML = "I would struggle to explain this";
+struggleBtn.setAttribute("data-skill", skill);
+struggleBtn.onclick = function() {
+var s = this.getAttribute("data-skill");
+selfRatings[s] = "struggle";
+highlightSelected(this);
+};
+
+block.appendChild(clearBtn);
+block.appendChild(struggleBtn);
+explainSection.appendChild(block);
+}
+};
+
+function highlightSelected(clickedBtn) {
+var skill = clickedBtn.getAttribute("data-skill");
+var siblingButtons = document.querySelectorAll(".rate-btn[data-skill='" + skill + "']");
+for (var i = 0; i < siblingButtons.length; i++) {
+siblingButtons[i].classList.remove("selected");
+}
+clickedBtn.classList.add("selected");
+}
+
 document.getElementById("check-btn").addEventListener("click", function() {
 var checkboxes = document.querySelectorAll("#skills-checklist input[type=checkbox]:checked");
 var userSkills = [];
@@ -69,6 +119,24 @@ output += "<li>" + haveSkills[i] + "</li>";
 output += "</ul>";
 } else {
 output += "<p>None yet, but every expert started here.</p>";
+}
+
+var struggledSkills = [];
+for (var i = 0; i < haveSkills.length; i++) {
+if (selfRatings[haveSkills[i]] === "struggle") {
+struggledSkills.push(haveSkills[i]);
+}
+}
+
+if (struggledSkills.length > 0) {
+output += "<div class='project-suggestion' style='border-left-color:#f59e0b;'><strong>Honesty Check:</strong> You marked ";
+for (var i = 0; i < struggledSkills.length; i++) {
+output += struggledSkills[i];
+if (i < struggledSkills.length - 1) {
+output += ", ";
+}
+}
+output += " as known, but noted you would struggle to explain " + (struggledSkills.length > 1 ? "these" : "this") + " clearly. Consider reviewing before interviews.</div>";
 }
 
 output += "<p class='result-missing'>Skills you are missing:</p>";
